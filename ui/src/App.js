5b88b88f5da9b55fd50671b636ba6a1185d2c248
Ganesh Pendyala <lak.gani@gmail.com>
2021-01-16 23:38:15 +0530

    Add bootstrap nav

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,58 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import 'bootstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+import { Counter } from "./features/counter/Counter";
+import Tasks from "./routes/Tasks";
+import Groups from "./routes/Groups";
+import Page404 from "./routes/Page404";
 
-function App() {
+// This site has 3 pages, all of which are rendered
+// dynamically in the browser (not server rendered).
+//
+// Although the page does not ever refresh, notice how
+// React Router keeps the URL up to date as you navigate
+// through the site. This preserves the browser history,
+// making sure things like the back button and bookmarks
+// work properly.
+
+export default function BasicExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav class="nav nav-pills d-flex flex-xs-column flex-sm-row justify-content-end">
+          <NavLink to="/" className="flex-xs-fill nav-link" activeClassName="active" exact>Tasks</NavLink>
+          <NavLink to="/groups" className="flex-xs-fill nav-link" activeClassName="active">Groups</NavLink>
+          <NavLink to="/counter" className="flex-xs-fill nav-link" activeClassName="active">Counter</NavLink>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <Tasks />
+          </Route>
+          <Route path="/groups">
+            <Groups />
+          </Route>
+          <Route path="/counter">
+            <Counter />
+          </Route>
+          <Route path="*">
+            <Page404 />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
